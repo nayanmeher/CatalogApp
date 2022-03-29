bfbff6423e76957dde1c829728e281a6261b1eea
nayanmeher <nayan.meher@kelltontech.com>
2022-03-29 15:40:40 +0530

    Added myhome

---
+++ lib/my_home.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class MyHome extends StatefulWidget {
+  @override
+  _MyHomeState createState() => _MyHomeState();
+}
+
+class _MyHomeState extends State<MyHome> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Catalog App'),
+      ),
+      body: Container(),
+      drawer: Drawer(),
+    );
+  }
+}
