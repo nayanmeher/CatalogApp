afb74ec3aa0d38fa7d98183a2c9c93b8026caf92
nayanmeher <nayan.meher@kelltontech.com>
2022-03-30 15:58:20 +0530

    base login page done. google font added.

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,24 @@
+
+import 'package:bloc_test1/pages/login_page.dart';
 import 'package:flutter/material.dart';
-import 'my_home.dart';
+import 'pages/my_home.dart';
+import 'package:google_fonts/google_fonts.dart';
 void main() {
   runApp(MaterialApp(
-    home: MyHome(),
+    themeMode: ThemeMode.light,
+    darkTheme: ThemeData(
+      brightness: Brightness.dark
+    ),
+    theme: ThemeData(
+      fontFamily: GoogleFonts.lato().fontFamily,
+      primarySwatch: Colors.deepPurple
+    ),
+    //initialRoute: '/home',
+    routes: {
+      "/":(context) => LoginPage(),
+      "/home": (context) => MyHome(),
+      "/login": (context) => LoginPage()
+    },
   ));
 }
 
